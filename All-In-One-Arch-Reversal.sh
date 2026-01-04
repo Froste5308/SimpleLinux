44fac1c2f0587c46d8332ef4eb7fd938a39b8b2a
Eric Luce <eluce11232011@gmail.com>
2026-01-03 20:04:57 -0500

    Update All-In-One-Arch-Reversal.sh

--- All-In-One-Arch-Reversal.sh
+++ All-In-One-Arch-Reversal.sh
@@ -5,6 +5,6 @@ sleep 15
 chsh -s /usr/bin/bash
 sudo pacman -Rns --noconfirm --quiet pipes.sh google-chrome brave-bin zen-browser-bin firefox balena-etcher ventoy-bin git curl wget fish android-tools android-udev heimdall gvfs gvfs-mtp cava figlet lolcat sl usbutils ptyxis cowsay fastfetch vim nano bat eza yay
 rm -rf /home/$USER/.config/fish
-sudo rm -rf /home/$USER/.config/*
+sudo rm -rf /home/$USER/.cache/*
 clear
 echo -e "\nAll Done! Now The Packages From All-In-One-Arch.sh Are Gone!\n"
