7aec1502c231b4200127b78499bf7a1ad9266159
Eric Luce <eluce11232011@gmail.com>
2026-01-03 19:58:56 -0500

    Update All-In-One-Arch-Reversal.sh

--- All-In-One-Arch-Reversal.sh
+++ All-In-One-Arch-Reversal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 figlet "Removing!"
+echo -e "\nTHERE IS NO CONFIRMATION, PLEASE MAKE SURE YOU ARE RUNNING THIS ON A FRESH ARCH INSTALL AFTER RUNNING All-In-One-Arch.sh BECAUSE THIS IS MEANT TO REMOVE EVERYTHING FROM IT AND REMOVES ALL THE DEPENDENCIES!!!\n"
+sleep 15
 sudo pacman -Rns --noconfirm --quiet pipes.sh google-chrome brave-bin zen-browser-bin firefox balena-etcher ventoy-bin git curl wget fish android-tools android-udev heimdall gvfs gvfs-mtp cava figlet lolcat sl usbutils ptyxis cowsay fastfetch vim nano bat eza yay
 rm -rf /home/$USER/.config/fish
 sudo rm -rf /home/$USER/.config/*
